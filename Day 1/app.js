74540c482b5b9a2996ee849de5ff84fa543387a5
lampertc <christopherlampert@rentalcars.com>
2020-12-01 16:51:57 +0000

    Day 1 challenge 1 complete

---
+++ Day 1/app.js
@@ -0,0 +1,13 @@
+const report = require ('./expenseReport.js');
+
+for (let i=0; i < report.length; i++){
+    for (let y = 0; y < report.length; y++) {
+        let calculation = report[i] + report[y];
+
+        if (calculation === 2020) {
+            console.log(report[i] * report[y]);
+            break;
+        }
+        y++;
+    }
+}
