ac1e56ab1d2c7d5ba8ef5d50e7c62579427809d4
lampertc <christopherlampert@rentalcars.com>
2020-12-04 09:12:01 +0000

    completed part 1 of day 3

---
+++ day-3/part-1.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const lines = fs.readFileSync('map.txt', {encoding: 'utf-8'}).split('\n');
+let forestMap = [];
+let currentPosition = 3;
+let count = 0
+
+lines.forEach(line => {
+    let newLine = line.repeat(32);
+    newLine = newLine + '!';
+    forestMap.push(newLine);
+});
+
+console.log(forestMap)
+
+for(let i = 1; i < forestMap.length; i++) {
+    if(forestMap[i][currentPosition] === '#') {
+        count += 1;
+    }
+    currentPosition += 3;
+}
+
+console.log(count);
+
